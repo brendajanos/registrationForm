9a12b0df1d675fab7201847d463ca0d09199c8a1
brendajanos <brenda.janos@icloud.com>
2022-03-13 16:20:10 +0100

    first registration form done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes, Navigate, BrowserRouter as Router } from 'react-router-dom'
 import RegistrationPage from '../pages/RegistrationPage'
 import ConfirmationPage from '../pages/ConfirmationPage'
+import PreferencesPage from '../pages/PreferencesPage'
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Navigate to="/registration"/>} />
                     <Route path="/registration" element={<RegistrationPage/>} />
+                    <Route path="/preferences" element={<PreferencesPage/>} />
                     <Route path="/email-confirmation" element={<ConfirmationPage/>} />
                 </Routes>
             </div>
--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { FormEventHandler } from 'react'
 
 const RegistrationPage = () => {
-  return (
-    <div>RegistrationPage</div>
-  )
+    const handleSubmit: FormEventHandler = (event) => {
+        event.preventDefault()
+
+        const form = event.target as HTMLFormElement
+
+        console.table({
+            email: form.email.value,
+            password: form.password.value,
+            birthdate: form.birthdate.value,
+            newsletter: form.newsletter.value,
+            cookies: form.cookies.value,
+        })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input id="email" name="email" type="email"></input>
+
+            <label htmlFor="password">Jelszó</label>
+            <input id="password" name="password" type="password"></input>
+
+            <label htmlFor="birthdate">Születési idő</label>
+            <input id="birthdate" name="birthdate" type="date"></input>
+
+            <label htmlFor="newsletter">Szeretnél hírleveleket?</label>
+            <input id="newletter" name="newsletter" type="checkbox"></input>
+
+            <label htmlFor="cookies">Hozzájárulsz az adatakezeléshez?</label>
+            <input id="cookies" name="cookies" type="checkbox"></input>
+
+            <input type="submit" value="Regisztrálás" />
+        </form>
+    )
 }
 
 export default RegistrationPage
